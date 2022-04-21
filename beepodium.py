9a5af1241a932334bf4642853e30cb967952bd1c
Gustavo Guedes <gustavoipguedes@gmail.com>
2022-04-21 09:30:02 -0300

    feat: podium generate

---
+++ beepodium.py
@@ -0,0 +1,42 @@
+from PIL import Image, ImageDraw
+import requests
+import io 
+
+def open_img_from_url(url):
+    response = requests.get(url)
+    image_bytes = io.BytesIO(response.content)
+    img = Image.open(image_bytes).resize((300, 300))
+    return img
+
+def get_profile_mask(size):
+    mask = Image.new('L', size, 0)
+    draw = ImageDraw.Draw(mask)
+    draw.ellipse((0, 0) + size, fill=255)
+    return mask
+
+def put_place(podium, user, place:int):
+    if place == 1:
+        pos = (860, 200)
+    elif place == 2:
+        pos = (400,300)
+    elif place == 3:
+        pos = (1330,350)
+    x,y = pos
+    print(user)
+    img = open_img_from_url(user['avatar'])
+    podium.paste(img, (x,y), get_profile_mask(img.size))
+
+def create_podium(user1, user2, user3):
+    imgPodium = Image.open("img/podio.jpg")
+
+    put_place(imgPodium, user1, 1)
+    put_place(imgPodium, user2, 2)
+    put_place(imgPodium, user3, 3)
+
+    bio = io.BytesIO()
+    bio.name = 'podio_tmp.jpeg'
+    imgPodium.save(bio, 'JPEG')
+    bio.seek(0)
+
+    return bio
+
